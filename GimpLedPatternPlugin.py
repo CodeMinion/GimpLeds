c631fc98f073a79afa983c669cf70bbef538e8d3
Frank E. Hernandez <hernandez.frank@gmail.com>
2020-04-02 13:38:13 -0400

    Added layer group support and generation of ReadMe.

--- GimpLedPatternPlugin.py
+++ GimpLedPatternPlugin.py
@@ -5,9 +5,9 @@ import os
 from gimpfu import *
 
 def generate_led_pattern(ledType, newimg, 
-               frameDelay, dir):
+               frameDelay, rowOrderType, dir):
     
-	
+	# TODO Consider allowing user to specify pattern name instead of using the file name.
 	filename, file_extension = os.path.splitext(newimg.name)
 	
 	constPattern = nameToConst(filename)
@@ -17,9 +17,15 @@ def generate_led_pattern(ledType, newimg,
 	
 	ledFrames = []
 	# Get layers in the newimg
-	imageLayers = newimg.layers
+	imageLayers = flatternGroups(newimg)
+	layerIndex = 0
 	for layer in imageLayers:
-		# TODO convert the layer name to a const name 
+		
+		# Skip the layers that are hidden / not visible
+		if not pdb.gimp_drawable_get_visible(layer):
+			continue
+		
+		# convert the layer name to a const name 
 		constLayer = nameToConst(layer.name)
 		
 		# Track the pattern name
@@ -48,7 +54,11 @@ def generate_led_pattern(ledType, newimg,
 					
 				# Track LED
 				pixelColors.append(pixelColor)
-					
+		
+		# Perform any processing needed on the pixel row.
+		pixelColors = processPixelRow(pixelColors, rowOrderType, rowOrderType)
+		
+		# TODO Depending on the row ordering flip this list. 	
 		# Track pattern.
 		ledFrame = {
 			KEY_FRAME_ID: constLayer,
@@ -81,6 +91,53 @@ def generate_led_pattern(ledType, newimg,
 def nameToConst(name):
 	outName = name.upper().replace(" ", "_").replace("#","")
 	return outName
+
+# Groups are layers composed of layser, so this will flatten 
+# all the groups into a single list of layers.
+# Note: Not to be confused with the actual GIMP command called flatten
+# which produces a single image with all visible layers.	
+def flatternGroups(parent):
+	
+	outLayers = []
+	
+	layers = parent.layers
+	
+	for layer in layers:
+		if  pdb.gimp_item_is_group(layer) and pdb.gimp_drawable_get_visible(layer):
+			# flatten the groups 
+			outLayers.extend( flatternGroups(layer))
+			pass
+		else:
+			outLayers.append(layer)
+	
+	return outLayers
+
+# Processing to support form common LED layouts.
+# Standard - Default, row-major. No changes needed.
+# Flip Odd - Reverses the pixels in the odd rows.
+# Flip Even - Reverses the pixels in the even rows.
+def processPixelRow(pixelList, rowOrder, rowPosition):
+	outPixelList = []
+	if rowOrder == ROW_PROCESSING_STANDARD:
+		# Do nothing
+		outPixelList = pixelList
+	elif rowOrder == ROW_PROCESSING_EVEN and rowPosition % 2 == 0:
+		pixelList.reverse()
+		outPixelList = pixelList
+	elif rowOrder == ROW_PROCESSING_ODD and rowPosition % 2 == 1:
+		pixelList.reverse()
+		outPixelList = pixelList
+		
+	return outPixelList	
+'''
+Row Processing Options
+'''
+# No change, continue default row-major order.
+ROW_PROCESSING_STANDARD = 0
+# Reverse pixel Order for odd rows.
+ROW_PROCESSING_ODD = 1 
+# Reverse pixel order for even rows.
+ROW_PROCESSING_EVEN = 2 	
 '''
 Code Generation Choices
 '''
@@ -218,6 +275,7 @@ class AdafruitNeoPixelStripCodeGenerator:
 		self.writeBaseLedPatternClass()
 		
 		# TODO Generate ReadMe file with simple instructions on how to include in existing script.
+		self.generateReadMe(patternId)
 		pass
 
 	# Dims a color by a given ratio. Because we are creating LED 
@@ -398,12 +456,61 @@ class {4} : public GimpLedPattern
 		self.getBasePatternClassName(), 
 		self.getGeneratedLedPatternClassName(patternId))
 		)
+	
+	# Generates a ReadMe file for ease of integration into an existing sketch. 
+	def generateReadMe(self, patternId):
+		outFilename = os.path.join(self.mOutDir, 'ReadMe_{0}.txt'.format(self.getGeneratedLedPatternClassName(patternId)))
+		readMeFile = open(outFilename, "w")
+		readMeFile.write("""
+// 1 - Include at the top of Arduino sketch under your other #include statements.
+#include "{0}.h"
+
+// 2 - Paste on top of setup() and under Adafruit NeoPixel declaration.
+// Note: This assumes you named your pixel strip 'strip' as in the Adafruit sample
+// from: https://learn.adafruit.com/adafruit-neopixel-uberguide?view=all#arduino-library-installation
+// If you named it differently used that name here instead of 'strip'
+{1} * {2} = new {0}(strip);
+
+// 3 - Paste inside loop() to run the pattern.
+{2}->playPattern();  
+  
+// 4 - Optional: Use this to stop the pattern while it is in the middle of running.
+//{2}->stopPattern();
+
+		""".format(self.getGeneratedLedPatternClassName(patternId),
+		self.getBasePatternClassName(),
+		self.getGeneratedLedPatternClassName(patternId).lower()))
+		readMeFile.close()
+		pass
 		
+'''
+End: Adafruit Code Generator
+'''
+
 		
 register(
     "python_fu_code_minion_led_pattern_generator",
     "Generates LED pattern from Image",
-    "Uses the pixels in the current image to generate an LED pattern for Arduino. Layers will be used as frames in the pattern.",
+    """Uses the pixels in the current image to generate an LED pattern for Arduino. Layers will be used as frames in the pattern and only visible layers are used. Layer Groups are also supported and will be included in the final patten if visible. 
+	
+UI Fields Help
+LED Type: Type of LEDs for which to generated the code for. 
+- Currently supporting: 
+-- Adafruit NeoPixel for Arduino
+	
+Image: The GIMP image to use as an imput for the generation of the LED pattern. 
+	
+Frame Delay: Delay in millisaconds of each frame. This is how long each layer of pixels will be shown for while the pattern is running. 
+	
+Row Order Type: This is the order in which the pixels translated to LED positions. Used to support common wiring forms for LED Matrices. 
+- Currently supporting: 
+-- Standard: This is row major.
+-- Flip Odd: This will flip the order of the pixels in the odd rows. First pixel will mapped to last LED and last pixel will map to the first LED.
+-- Flip Even: This will flip the order of the pixels in the even rows. First pixel will mapped to last LED and last pixel will map to the first LED.
+	
+Directory: Directory where the code will be placed once generation is complete. It is recommended to make this your Arduino sketch folder for convenience but not required. 
+	
+	""",
     "Frank E. Hernandez",
     "Frank E. Hernandez",
     "2020",
@@ -413,9 +520,9 @@ register(
         (PF_OPTION, "ledType", "LED Type", 0, ("Adafruit NeoPixel", "Coming Soon...")),
         (PF_IMAGE, "image", "Input image", None),
         (PF_SPINNER, "frameDelay", "Frame Delay (ms)", 200, (1, 80000, 1)),
-		# TODO Remove, not needed/used.
-		#(PF_FILE, "imagefile", "Image file", ""),
-        (PF_DIRNAME, "dir", "Directory", "/tmp")
+		(PF_OPTION, "rowOrderType", "Row Ordering", 0, ("Standard", "Flip Odd", "Flip Even")),
+        
+		(PF_DIRNAME, "dir", "Directory", "/tmp")
 
 		# Python-Fu Type, paramter-name, ui-text, default
     ],
